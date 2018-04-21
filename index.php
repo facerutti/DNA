77be22ebb3ade91e61191aa1556741ff7938f86b
joaodacouve <facerutti@gmail.com>
2018-04-21 10:43:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,259 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="IoT, Cloud, Automação, CFTV, TI, Virtulaização, Datacenter">
+    <meta name="description" content="A Techno Cloud atua no mercado de TIC a mais de 7 anos, sempre e Sempre com soluções eficazes, em sintonia com as tendências tecnológicas mundiais.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="./css/animate.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/linecons.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/feather.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    
+	<script src="./js/jquery-2.1.0.min.js"></script>
+	<script src="./js/bootstrap.min.js"></script>
+	<script src="./js/blocs.min.js"></script>
+	<script src="./js/lazysizes.min.js" defer></script>
+    <title>Techno Cloud TI | SC</title>
+
+    
+<!-- Google Analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-15346525-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-15346525-2');
+</script>
+
+<!-- Google Analytics END -->
+    
+</head>
+<body>
+<!-- Main container -->
+<div class="page-container">
+    
+<!-- bloc-0 -->
+<div class="bloc bgc-white l-bloc " id="bloc-0">
+	<div class="container bloc-sm">
+		<nav class="navbar row">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php"><img src="img/Logo_TC_sem_inovacoes.png" alt="logo" height="100" width="187" />Inovações em TI</a>
+				<button id="nav-toggle" type="button" class="ui-navbar-toggle navbar-toggle menu-icon-thin-bars" data-toggle="collapse" data-target=".navbar-1">
+					<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse navbar-1 nav-special sidebar-nav">
+				<ul class="site-navigation nav navbar-nav">
+					<li>
+						<a href="index.php" class="ltc-shamrock-green">Home</a>
+					</li>
+					<li>
+						<a href="produtos_0.php" class="ltc-shamrock-green">Produtos</a>
+					</li>
+					<li>
+						<a href="contato.php" class="ltc-shamrock-green">Contato</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+</div>
+<!-- bloc-0 END -->
+
+<!-- bloc-1 -->
+<div class="bloc bgc-white tc-white" id="bloc-1">
+	<div class="video-bg-container">
+		<video class="bloc-video" preload="auto" loop="loop" autoplay="autoplay">
+			<source src="vid/people%20high%20tech%20moving.mp4" type="video/mp4" />
+		</video>
+	</div>
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-sm-12 col-md-6 col-md-offset-3">
+				<div class="panel panel-home panell-home-paragraph">
+					<div class="panel-heading">
+						<h1 class="mg-md tc-white">
+							Nós Pensamos o Futuro
+						</h1>
+					</div>
+					<div class="panel-body">
+						<p class="text-center paragraph-home">
+							A Technocloud prioriza tecnologias avançadas para Gestão de Informações e Comunicações<br>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-1 END -->
+
+<!-- bloc-2 -->
+<div id="bloc-2" class="bloc feature-list tc-black animSpeed10 none animLoopInfinite bg-Sky-High bgc-white b-divider l-bloc">
+	<div class="container bloc-md">
+		<div class="row row-tc3">
+			<div class="col-sm-12">
+				<h1 class="text-center  tit-servicos tc-light-gray mg-lg">
+					Serviços &amp; Inovações
+				</h1>
+				<div class="text-center">
+					<a href="padroes_mundiais.php" class="btn btn-sm botao-padroes btn-shamrock-green">Padrões e Processos --&gt;</a>
+				</div>
+			</div>
+		</div>
+		<div class="row voffset" id="serviços-e-inovações">
+			<div class="col-sm-6">
+				<div class="col-sm-3 col-md-3 col-sm-push-9">
+					<div class="text-center">
+						<a href="video_analise.php"><span class="li_video icontc threeD-t bounce-hvr icon-round icon-md"></span></a>
+					</div>
+				</div>
+				<div class="col-sm-9 col-sm-pull-3">
+					<h3 class="text-right mg-md blocs-mob-center-text  tc-white">
+						<strong>&nbsp;Vídeo Análise e&nbsp;CFTV</strong>
+					</h3>
+					<p class="text-right blocs-mob-center-text  mg-lg">
+						Monitoraramento térmico, análise de perimetro, reconhecimento facial, rastreamento de movimentos.
+					</p><a href="video_analise.php" class="btn btn-sm botao-tc btn-clean pull-right btn-d">Veja Mais --&gt;</a>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="col-sm-3">
+					<div class="text-center">
+						<div class="text-center">
+							<a href="daas.php"><span class="li_display icontc icon-round bounce-hvr icon-md animated"></span></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-9">
+					<h3 class="mg-md blocs-mob-center-text  text-left tc-white">
+						<strong>DaaS - Desktop as a Service</strong>
+					</h3>
+					<p class="blocs-mob-center-text  text-left">
+						Virtualização massiva de desktops, com camada de gerência totalmente integrada ao Hardware. Latência quase nula, inclusive para redes WAN
+					</p><a href="daas.php" class="btn btn-sm botao-tc btn-clean pull-right btn-d">Veja Mais --&gt;</a>
+				</div>
+			</div>
+		</div>
+		<div class="row voffset">
+			<div class="col-sm-6">
+				<div class="col-sm-3 col-md-3 col-sm-push-9">
+					<div class="text-center icon-sm">
+						<div class="text-center">
+							<a href="inteligencia.php"><span class="li_settings icontc icon-round bounce-hvr icon-md animated"></span></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-9 col-sm-pull-3">
+					<h3 class="text-right mg-md blocs-mob-center-text  tc-white">
+						<strong>Inteligência &amp; Automação</strong>
+					</h3>
+					<p class="text-right blocs-mob-center-text " id="inicio-servicos">
+						Amostragem online com sensores, processamento e acionamento de alarmes bombas ou funcionalidades de software
+					</p><a href="inteligencia.php" class="btn btn-sm botao-tc btn-clean pull-right btn-d">Veja Mais --&gt;</a>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="col-sm-3">
+					<div class="text-center">
+						<div class="text-center">
+							<a href="datacenter.php"><span class="li_data icontc icon-round bounce-hvr icon-md animated"></span></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-9">
+					<h3 class="mg-md blocs-mob-center-text  text-left tc-white">
+						<strong>Infraestrutura &amp; Datacenter</strong>
+					</h3>
+					<p class="blocs-mob-center-text  text-left">
+						Convergência, OpenStack, HPC,HA e DR
+					</p><a href="datacenter.php" class="btn btn-sm botao-tc btn-clean pull-right btn-d">Veja Mais --&gt;</a>
+				</div>
+			</div>
+		</div>
+		<div class="row voffset">
+			<div class="col-sm-6">
+				<div class="col-sm-3 col-md-3 col-sm-push-9">
+					<div class="text-center">
+						<div class="text-center">
+							<a href="iot.php"><span class="li_world icontc icon-round bounce-hvr icon-md animated"></span></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-9 col-sm-pull-3">
+					<h3 class="text-right mg-md blocs-mob-center-text  tc-white">
+						<strong>Internet das Coisas</strong>
+					</h3>
+					<p class="text-right blocs-mob-center-text ">
+						Comunicão Ubíqua, M2M, &nbsp;Integração dos dados para correlações e previsões.
+					</p><a href="iot.php" class="btn btn-sm botao-tc btn-clean pull-right btn-d">Veja Mais --&gt;</a>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="col-sm-3">
+					<div class="text-center">
+						<div class="text-center">
+							<a href="segurança.php"><span class="li_lock icontc icon-round flip-hvr icon-md"></span></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-9">
+					<h3 class="mg-md blocs-mob-center-text  text-left tc-white">
+						<strong>Segurança da Informação</strong>
+					</h3>
+					<p class="blocs-mob-center-text  text-left">
+						A disseminação exponencial de equipamentos por usuário é preocupante. As soluções Techno Cloud mitigam riscos. &nbsp;
+					</p><a href="segurança.php" class="btn btn-sm botao-tc btn-clean pull-right btn-d">Veja Mais --&gt;</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-2 END -->
+
+<!-- ScrollToTop Button -->
+<a class="bloc-button btn btn-d scrollToTop" onclick="scrollToTarget('1')"><span class="fa fa-chevron-up"></span></a>
+<!-- ScrollToTop Button END-->
+
+
+<!-- Footer - bloc-12 -->
+<div class="bloc bgc-white bloc-bg-texture texture-paper tc-black l-bloc" id="bloc-12">
+	<div class="container bloc-sm">
+		<div class="row">
+			<div class="col-sm-4">
+				<div class="text-center">
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="text-center">
+					<h2 class="text-center  tc-black mg-clear rodape">
+						Techno Cloud&nbsp;Inovações em TI
+					</h2>
+					<p class=" mg-clear text-w-90">
+						Rua Abílio Costa, 355/1 <br>Sta.Mônica Florianópolis SC <br>88035-150 f. (48)3029-0872
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="text-center">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Footer - bloc-12 END -->
+
+</div>
+<!-- Main container END -->
+    
+
+</body>
+</html>
